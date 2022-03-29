105e9b219f03641219ca356ccb1e5f3e904f70d9
George Araujo <georgepovoa12@gmail.com>
2022-03-29 16:40:39 -0300

    Comit4

--- startup/settings.py
+++ startup/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -139,3 +139,10 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/profile-picker'
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
