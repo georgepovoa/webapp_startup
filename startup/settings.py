5dee015a67ad23ee93fc21538675daba3a81363c
George Araujo <georgepovoa12@gmail.com>
2022-03-29 17:14:17 -0300

    agora vai

--- startup/settings.py
+++ startup/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -140,12 +140,5 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/profile-picker'
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 django_heroku.settings(locals())
