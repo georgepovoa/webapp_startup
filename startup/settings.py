00368db8b00693cb58354d66ce695317d0cbb6c8
georgeceub <georgepovoa12@gmail.com>
2022-05-26 10:54:48 -0300

    Alteração de cors

--- startup/settings.py
+++ startup/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'crispy_forms',
     'frontend.apps.FrontendConfig',
-    
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,9 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'startup.urls'
+
+CORS_ALLOW_ALL_ORIGINS: True
 
 TEMPLATES = [
     {
